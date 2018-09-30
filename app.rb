94792e89c207ec27fae17cd25be96edc729ba580
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-30 23:50:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,12 +11,13 @@ class App < Sinatra::Base
           erb :reversed
   end
 
-  get '/friends' do
     # Write your code here!
+    get '/friends' do
+
   @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
 
   erb :friends
-end
+  end
   end
 
 
